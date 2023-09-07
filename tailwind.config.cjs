172d51785827a3203340b97274f02c69e23a2c32
Franco M <damianmaciel0@gmail.com>
2023-09-06 21:10:49 -0300

    fix: optimized images and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				sans: ['Quicksand', ...defaultTheme.fontFamily.sans]
       },
       colors: {
         primary: 'rgba(var(--primary) / <alpha-value>)',
